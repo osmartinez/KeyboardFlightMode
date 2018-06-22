cd104060a6703ed2ae834c756b08864fd556a09c
Oscar <PC@PC-PC>
2018-06-22 15:23:10 +0200

    class renaming

--- KeyboardPiloting/main.cpp
+++ KeyboardPiloting/main.cpp
@@ -10,37 +10,63 @@ float roll = 1500;
 
 bool finish = false;
 
+/// <summary>
+/// Exported funciton to receive the throttle value
+/// </summary>
+/// <returns>throttle</returns>
 EXPORT float GetThrottle() {
 	return throttle;
 }
 
+/// <summary>
+/// Exported funciton to receive the yaw value
+/// </summary>
+/// <returns>yaw</returns>
 EXPORT float GetYaw() {
 	return yaw;
 }
 
+/// <summary>
+/// Exported funciton to receive the pitch value
+/// </summary>
+/// <returns>pitch</returns>
 EXPORT float GetPitch() {
 	return pitch;
 }
 
+/// <summary>
+/// Exported funciton to receive the roll value
+/// </summary>
+/// <returns>roll</returns>
 EXPORT float GetRoll() {
 	return roll;
 }
 
+/// <summary>
+/// Exported funciton stop the program externally
+/// </summary>
 EXPORT void Stop() {
 	finish = true;
 }
 
-EXPORT int Initialize(float sensibility,float minthrottle, float maxthrottle) {
+/// <summary>
+/// Initializes the program to catch the pressed keys
+/// </summary>
+/// <param name="sensitivity">throttle shift based on cps </param>
+/// <param name="minthrottle">minimun allowed throttle</param>
+/// <param name="maxthrottle">maximum allowed throttle</param>
+/// <returns>0 when finish</returns>
+EXPORT int Initialize(float sensitivity,float minthrottle, float maxthrottle) {
 	float res[4];
 	res[0] = 1000;
 	res[1] = res[2] = res[3] = 1500;
 	finish = false;
-	float factor = sensibility;
+	float factor = sensitivity;
 
 	while (!finish) {
 
 		// throttle
-		if (GetKeyState('W') & 0x8000/*check if high-order bit is set (1 << 15)*/)
+		if (GetKeyState('W') & 0x8000)
 		{
 			if (res[0] + factor > maxthrottle) {
 				res[0] = maxthrottle;
@@ -50,7 +76,7 @@ EXPORT int Initialize(float sensibility,float minthrottle, float maxthrottle) {
 			}
 		}
 
-		if (GetKeyState('S') & 0x8000/*check if high-order bit is set (1 << 15)*/)
+		if (GetKeyState('S') & 0x8000)
 		{
 			if (res[0] - factor < minthrottle) {
 				res[0] = minthrottle;
